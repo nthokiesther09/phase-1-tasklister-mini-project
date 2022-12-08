d7c6348f78bacf43dd18d6bcab052f7ff1e48635
nthokiesther09 <nthokiesther09@gmail.com>
2022-12-08 15:22:29 +0300

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (m) => {
+    m.preventDefault();
+    const newTask = document.getElementById("new-task-description").value
+    console.log(newTask);
+    builtdtask(newTask);
+  })
 });
+function builtdtask (task){
+  const li = document.createElement("li")
+  li.textContent = task
+  const button = document.createElement("button")
+  button.addEventListener('click', handledelete);
+  button.textContent = 'X';
+  li.appendChild(button)
+const theList =document.querySelector('#tasks')
+theList.appendChild(li)
+}
+function handledelete(m){
+  m.target.parentNode.remove();
+}
+
